340264822eed4895a1cf0de6b2b2dfe9b9c6ac63
CaptainHighlander <tedesco96riccardo@gmail.com>
2020-10-22 12:01:03 +0200

    The prototype version is almost completed

--- Server/tcp_connection.cpp
+++ Server/tcp_connection.cpp
@@ -10,6 +10,12 @@ TCP_Connection::pointer TCP_Connection::Create(boost::asio::io_context& io_conte
 #pragma endregion
 
 #pragma region Public members:
+TCP_Connection::~TCP_Connection(void)
+{
+    std::cout << "[DEBUG] Distruzione connessione" << std::endl;
+    this->Disconnect();
+}
+
 tcp::socket& TCP_Connection::GetSocket(void)
 {
     return this->socketServer;
@@ -17,35 +23,50 @@ tcp::socket& TCP_Connection::GetSocket(void)
 
 void TCP_Connection::Start(void)
 {
-    const string loginRequestMsg = "Please, insert <username>and <password>\n";
-    this->m_message = loginRequestMsg;
+    const string loginRequestMsg = "AUTH REQUEST\n";
+    this->outgoingMessage = loginRequestMsg;
 
-    // When initiating the asynchronous operation, and if using boost::bind(), we must specify only the arguments
-    // that match the handler's parameter list. Currently, in this code, both of the argument placeholders
-    // (boost::asio::placeholders::error and boost::asio::placeholders::bytes_transferred)
-    // could potentially have been removed, since they are not being used in handle_write().
+    // When initiating the asynchronous operation, and if using boost::bind(),
+    // we must specify only the arguments that match the handler's parameter list.
     // Call boost::asio::async_write() to serve the data to the client.
-    // We are using boost::asio::async_write(), rather than ip::tcp::socket::async_write_some(), to ensure that the entire block of data is sent.
-    boost::asio::async_write(this->socketServer, boost::asio::buffer(this->m_message),
+    // We are using boost::asio::async_write(), rather than ip::tcp::socket::async_write_some(),
+    // to ensure that the entire block of data is sent.
+    boost::asio::async_write(this->socketServer, boost::asio::buffer(this->outgoingMessage),
                              boost::bind(&TCP_Connection::HandleWrite, shared_from_this(),
-                             boost::asio::placeholders::error,
-                             boost::asio::placeholders::bytes_transferred));
+                             boost::asio::placeholders::error, boost::asio::placeholders::bytes_transferred));
 }
 #pragma endregion
 
 #pragma region Private members:
-TCP_Connection::TCP_Connection(boost::asio::io_context& io_context) : socketServer(io_context) { }
+TCP_Connection::TCP_Connection(boost::asio::io_context& io_context) : socketServer(io_context), failedLoginAttempts(0)
+{
+}
+
+void TCP_Connection::Disconnect(void)
+{
+    this->socketServer.close();
+}
 
 void TCP_Connection::HandleWrite(const boost::system::error_code& error, size_t bytes_transferred)
 {
-    //TMP
-    std::cout << "write completed" << std::endl;
-    //this->m_message = "test\n";
-    /*
-    boost::asio::async_write(this->socketServer, boost::asio::buffer(this->m_message),
-                             boost::bind(&TCP_Connection::HandleWrite, shared_from_this(),
-                                         boost::asio::placeholders::error,
-                                         boost::asio::placeholders::bytes_transferred));
-    */
+    std::cout << "[DEBUG] Write operation completed; transferred " << bytes_transferred << "bytes" << std::endl;
+    if (this->associatedUserID.empty() == true && this->failedLoginAttempts < this->MAX_NUMBER_OF_FAILED_LOGINS)
+    {
+        boost::asio::async_read_until(this->socketServer, boost::asio::dynamic_buffer(incomingMessage), '\n',
+                          boost::bind(&TCP_Connection::HandleRead, shared_from_this(),
+                                      boost::asio::placeholders::error, boost::asio::placeholders::bytes_transferred));
+        return;
+    }
+}
+
+void TCP_Connection::HandleRead(const boost::system::error_code& error, size_t bytes_transferred)
+{
+    if (this->associatedUserID.empty() == true)
+    {
+        // Popping last character "\n"
+        this->incomingMessage.pop_back();
+    }
+    std::cout << this->incomingMessage << std::endl;
+    std::cout << "[DEBUG] Read operation completed; transferred " << bytes_transferred << "bytes" << std::endl;
 }
 #pragma endregion
--- Server/tcp_connection.h
+++ Server/tcp_connection.h
@@ -15,23 +15,33 @@ class TCP_Connection : public boost::enable_shared_from_this<TCP_Connection>
 {
 public:
     typedef boost::shared_ptr<TCP_Connection> pointer;
-
     TCP_Connection(const TCP_Connection& other) = delete;
     void operator = (const TCP_Connection& other) = delete;
+    ~TCP_Connection(void);
 
     static pointer Create(boost::asio::io_context& io_context);
 
     [[nodiscard]] tcp::socket& GetSocket(void);
 
     void Start(void);
-
 private:
     explicit TCP_Connection(boost::asio::io_context& io_context);
+
+    void Disconnect(void);
 
     ///HandleWrite() is responsible for any further actions for this client connection.
     void HandleWrite(const boost::system::error_code& error, size_t bytes_transferred);
 
+    ///HandleRead() is responsible for any further actions for this client connection.
+    void HandleRead(const boost::system::error_code& error, size_t bytes_transferred);
+
     tcp::socket socketServer;
-    ///The data to be sent is stored in the class member m_message as we need to keep the data valid until the asynchronous operation is complete.
-    string m_message;
+    ///The data to be sent is stored in the class member outgoingMessage as we need to keep the data valid until the asynchronous operation is complete.
+    string outgoingMessage;
+    ///The data to be received is stored in the class member incomingMessage as we need to keep the data valid until the asynchronous operation is complete.
+    string incomingMessage;
+    ///The user who has logged into this connection.
+    string associatedUserID;
+    uint8_t failedLoginAttempts;
+    const uint8_t MAX_NUMBER_OF_FAILED_LOGINS = 3;
 };
